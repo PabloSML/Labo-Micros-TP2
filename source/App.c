ba56f574a8cfa39f4c20dd658701180f046ef2e8
PabloSmolkin <psmolkin@itba.edu.ar>
2021-09-13 11:10:35 -0300

    Probando mag tmb

--- source/App.c
+++ source/App.c
@@ -145,6 +145,29 @@ void App_Init (void)
 /* Función que se llama constantemente en un ciclo infinito */
 void App_Run (void)
 {
+
+  //  if(magreader_hasEvent())
+  //   {
+  //       switch(magreader_getEvent())
+  //       {
+  //           case MAGREADER_cardsliding:
+  //             ledOn(LED_3);
+  //             ledOff(LED_2);
+  //             ledOff(LED_1);
+  //           break;
+  //           case MAGREADER_carderror:
+  //             ledOff(LED_3);
+  //             ledOn(LED_2);
+  //             ledOff(LED_1);
+  //           break;
+  //           case MAGREADER_cardUpload:
+  //             ledOff(LED_3);
+  //             ledOff(LED_2);
+  //             ledOn(LED_1);
+  //           break;
+  //       }
+  //   }
+
   run_logic_module();
   
 }
